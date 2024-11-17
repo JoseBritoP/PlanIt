f31dceda68a0cf88d26b6dd0a87d714c03dc4c06
José Brito <josealbritose@gmail.com>
2024-11-17 00:04:48 -0300

    types

---
+++ src/types/index.d.ts
@@ -0,0 +1,9 @@
+import { CreateUserParams,UpdateUserParams } from "./user";
+import { CreateEventParams,DeleteEventParams,Event,GetAllEventsParams,GetEventsByUserParams,GetRelatedEventsByCategoryParams,UpdateEventParams } from "./event";
+import { CreateCategoryParams } from "./category";
+import { CheckoutOrderParams,CreateOrderParams,GetOrdersByEventParams,GetOrdersByUserParams } from "./order";
+import { UrlQueryParams,RemoveUrlQueryParams,SearchParamProps } from "./url";
+
+export {
+  CheckoutOrderParams,CreateCategoryParams,CreateEventParams,CreateOrderParams,CreateUserParams,DeleteEventParams,Event,GetAllEventsParams,GetEventsByUserParams,GetOrdersByEventParams,GetOrdersByUserParams,GetRelatedEventsByCategoryParams,RemoveUrlQueryParams,SearchParamProps,UpdateEventParams,UpdateUserParams,UrlQueryParams
+}
