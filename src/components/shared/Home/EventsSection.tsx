941d845d3e774804ec4dddd99519aaafcfd68475
José Brito <josealbritose@gmail.com>
2024-11-17 03:41:41 -0300

    EventsSection P

---
+++ src/components/shared/Home/EventsSection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function EventsSection() {
+  return (
+    <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+      <h2 className="h2-bold">
+        Trust by <br /> Thousands of Events
+      </h2>
+
+      <div className="flex w-full flex-col gap-5 md:flex-row"></div>
+    </section>
+  );
+}
