b9b1b29837481da9890eee391c3950f1401e7f9c
José Brito <josealbritose@gmail.com>
2024-11-17 01:00:30 -0300

    ClerkProviderInLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,20 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({
-  subsets:['latin'],
-  weight:['400','500','600','700'],
-  variable:'--font-poppins'
-})
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "PlanIt",
   description: "Manage your events",
-  icons:{
-    icon:'/assets/images/logo.svg'
-  }
+  icons: {
+    icon: "/assets/images/logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -22,12 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${poppins.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${poppins.variable} antialiased`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
