8236fa29c024aab742ab65d4c80863ef1701af1a
José Brito <josealbritose@gmail.com>
2024-11-17 01:00:36 -0300

    Layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
 import React from "react";
 
 export default function RootLayout({
