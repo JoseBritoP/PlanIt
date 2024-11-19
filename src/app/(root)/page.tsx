e39867589e49d2062cc143ab19d9f35da2309194
José Brito <josealbritose@gmail.com>
2024-11-18 22:06:38 -0300

    HomePage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,24 @@
+import EventsSection from "@/components/shared/Home/EventsSection";
 import HeroSection from "@/components/shared/Home/HeroSection";
+import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 import React from "react";
 
-export default function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6,
+  });
   return (
     <>
       <HeroSection />
+      <EventsSection events={events} page={page} />
     </>
   );
 }
